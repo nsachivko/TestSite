e735d35f8b08b929b7aa356a3f2b688fd398b7ce
Nikita Sachivko <n_sachivko@oxfordproperties.com>
2022-01-12 17:26:42 -0500

    1

--- modules/PostDetails.js
+++ modules/PostDetails.js
@@ -1,34 +1,24 @@
-import React from 'react';
-import { renderHTML } from "../agility/utils"
+import React from "react";
+import { renderHTML } from "../agility/utils";
+import parse from "html-react-parser";
 
 const PostDetails = ({ dynamicPageItem, item }) => {
 
-	let post = dynamicPageItem;
+  const replaceGlobally = (original, searchTxt, replaceTxt) => {
+    const regex = new RegExp(searchTxt, "g")
+    return original.replace(regex, replaceTxt)
+  };
 
-	return (
-		<section className="max-w-screen-xl mx-auto px-4 sm:px-6 py-6">
+  const fields = dynamicPageItem.fields;
 
-			<div className="text-center relative">
-				<h1 className="text-3xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-					{post.fields.title}
-				</h1>
-				<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto ">
-					Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-				</p>
-
-				{post.fields.image &&
-
-					<img className="mx-auto my-10" src={post.fields.image.url + '?w=860'} alt={post.fields.image.label} />
-
-				}
-
-				<div className="mx-20 mt-10 text-left" dangerouslySetInnerHTML={renderHTML(post.fields.details)}></div>
-
-			</div>
-		</section >
-	)
-
-}
-
+  let postsString = replaceGlobally(fields.content, "<h2>", '<h2 className="mt-10">')
+  postsString = replaceGlobally(postsString, "<img", '<img className="rounded-md object-cover h-96 w-192 mr-5"')
+  
+  return (
+    <div className="container mx-auto px-4 mt-5">
+      {parse(postsString)}
+    </div>
+  );
+};
 
 export default PostDetails;
