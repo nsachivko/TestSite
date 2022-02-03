c2573df7bcbad7e3cab7e785ffb1f089dc4bc458
nsachivko <nsachivko@myseneca.ca>
2022-02-03 17:24:43 -0500

    security fix

--- pages/[...slug].js
+++ pages/[...slug].js
@@ -1,50 +1,45 @@
-import React, { Component } from 'react';
-import { getAgilityPageProps, getAgilityPaths } from '../agility/agility.node'
-import { handlePreview } from '../agility/agility.browser'
-import Layout from '../components/Layout'
+import React, { Component } from "react"
+import { getAgilityPageProps, getAgilityPaths } from "../agility/agility.node"
+import { handlePreview } from "../agility/agility.browser"
+import Layout from "../components/Layout"
 
 class AgilityPage extends Component {
-	render() {
-		handlePreview();
+  render() {
+    handlePreview()
 
+    //this runtime error ONLY hapens in preview mode...
+    if (this.props.previewError) {
+      return (
+        <div>
+          <div>{this.props.previewError}</div>
+          <div>{this.props.previewError}</div>
+        </div>
+      )
+    }
 
-		//this runtime error ONLY hapens in preview mode...
-		if (this.props.previewError) {
-			return <div>
-				<div>{this.props.previewError}</div>
-				<div>{this.props.previewError}</div>
-			</div>
-		}
-
-		return (
-			<Layout {...this.props} />
-		)
-	}
+    return <Layout {...this.props} />
+  }
 }
 
 export async function getStaticProps(context) {
-
-	try {
-
-		const props = await getAgilityPageProps({ context });
-		return {
-			props: props
-		}
-	} catch (e) {
-
-		return {
-			props: { previewError: e.message, stack: e.stack }
-		}
-	}
+  try {
+    const props = await getAgilityPageProps({ context })
+    return {
+      props: props,
+    }
+  } catch (e) {
+    return {
+      props: { previewError: e.message, stack: e.stack },
+    }
+  }
 }
 
 export async function getStaticPaths() {
-	const paths = await getAgilityPaths();
-	return {
-		paths: paths,
-		fallback: false
-	}
+  const paths = await getAgilityPaths()
+  return {
+    paths: paths,
+    fallback: false,
+  }
 }
 
 export default AgilityPage
-
